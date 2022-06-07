41330550dbbddaecb1f7a148e396f0985e7f6461
abrash <rajirasheed39@gmail.com>
2022-06-07 16:52:29 -0700

    1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#inlude "main.h"                                                                                                                        
+                                                                                                                                        
+/**                                                                                                                                     
+ *  *                                                                                                                                      
+ *   * main - Entry point                                                                                                                   
+ *    * Return: Nothing, return void                                              */                                                                                                                                     
+                                                                                                                                        
+int void(main)                                                                                                                          
+{                                                                                                                                       
+	                                                                                                                                        
+	        _putchar('_');                                                                                                                  
+		        _putchar('p');                                                                                                                  
+			        _putchar('u');                                                                                                                  
+				        _putchar9      
